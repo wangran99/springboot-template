1e71d5c28132c4c6e31cdef685a8337b31659217
汪然 <wangran050@chinasoftinc.com>
2021-02-08 14:32:47 +0800

    通用异常

---
+++ src/main/java/com/chinasoft/example/exception/CommonException.java
@@ -0,0 +1,19 @@
+package com.chinasoft.example.exception;
+
+import com.github.wangran99.welink.api.client.openapi.model.IException;
+
+public class CommonException extends RuntimeException{
+
+    private int  code;
+    private String msg;
+
+    public CommonException(IException iException) {
+        super(iException.getDesc());
+        this.msg = iException.getDesc();
+        this.code = iException.getCode();
+    }
+    public CommonException() {
+        super("CommonException");
+    }
+
+}
